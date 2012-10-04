eb605f7348a8a20513987761f60ce83f7a61514d
JessMehaffey <jessmehaffey@fullsail.edu>
2012-10-04 17:50:38 -0300

    While Loop Construction. For Loop Finished?

--- Mehaffey_Jessica_Assignment2/Mehaffey_Jessica_Assignment2.js
+++ Mehaffey_Jessica_Assignment2/Mehaffey_Jessica_Assignment2.js
@@ -14,7 +14,7 @@ var arrFlavors = ["Vanilla", "Chocolate", "Oreo", "Red Velvet", "Cookie Dough"];
 var arrToppings = ["Once you decorate them, roll them in rainbow sprinkles!", "Chocolate chips go in the batter- don\'t forget!", "Half of an Oreo cookie goes in the liner before you add the batter!", "This is the only cupcake that gets cream cheese icing!", "Don\'t eat the raw cookie dough! ...I\'m kidding. Give me a scoop!",];
 var addOutcome;
 
-// Main Code Section 1
+// Beginning Text
 console.log("Good Morning! Hope you are ready to get coated in batter! Things are getting hectic around here, and we need everybody wide awake and ready to work!");
 console.log("My name is " + strName + " and I\'m in charge this morning! Get the eggs out of the fridge and meet me by the mixers, ASAP!");
 
@@ -25,24 +25,31 @@ var addition = function(firstNumber, secondNumber){
 		return outcome;
 };
 
-
 // While Loop
-var countDown = function(cupcakes){
-
-	for (var flavorsFinished = 0; flavorsFinished < arrFlavors.length; flavorsFinished++) {
-		console.log( + " flavors of cupcakes still need baked and decorated!");
-		console.log("When you think about it, " + flavorsFinished + " flavors of cupcakes really won\'t take too long to bake!");
-		flavorsFinished++; // Value of flavorsFinished increases by 1 each time
+// Note to self: Fix this and figure it out!
+var flavorCountDown = function(flavors)
+	while (flavors < 5){
+		console.log(flavors + " left to bake!");
 		
-		
-		if (cupcakes > 0){
-			console.log(flavorsFinished + "flavors left to bake!");
-		} else {
-			console.log("Put down your oven mitt- we\'re done baking for the day!");
-		};
-	};
 	
+	};
+
+
+
+// For Loop	
+for (var flavorsFinished = 0; flavorsFinished < arrFlavors.length; flavorsFinished++) {
+	console.log( + " flavors of cupcakes still need baked and decorated!");
+	console.log("When you think about it, " + flavorsFinished + " flavors of cupcakes really won\'t take too long to bake!");
+	flavorsFinished++; // Value of flavorsFinished increases by 1 each time
+		
+		
+	if (flavors === 5){
+		console.log("Put down your oven mitt- we\'re done baking for the day!")
+	} else {
+		console.log(flavorsFinished + "flavors left to bake! Get on it!");
+	};
 };
+	
 
 
 // Loop
@@ -51,5 +58,6 @@ for (var i=0; j = arrFlavors.length; i < j; i++) {
 };
 
 
-// Main Code Section 2
+// Main Code
+bakeAllCupcakes(arrFlavors, arrToppings);
 console.log("I can\'t believe we have baked " + addition(100 + 4) + "cupcakes already! We\'ve been busy bees!");
