09d95f648ad361e57db63046feec7a1688c2bcf6
JessMehaffey <jessmehaffey@fullsail.edu>
2012-10-10 16:31:07 -0300

    Prepared and Added Guidelines For Code.
    Mostly just //comments and //comments and //comments.

--- Mehaffey_Jessica_Assignment3/Mehaffey_Jessica_Assignment3.js
+++ Mehaffey_Jessica_Assignment3/Mehaffey_Jessica_Assignment3.js
@@ -1,1 +1,19 @@
-alert("JavaScript works!");
+// Jessica Mehaffey
+// SDI 1210
+// Project 3
+// Javascript: Free Form Flow Chart
+
+// Personal Notes:
+// Make sure ";" ends every line!
+// Arrays start with place 0!
+// Always add \escapes when including quotes and apostrophes!
+
+// Set Variables
+// var ______ = ""
+
+
+// Output Log
+// This contains the console.log text that will show up upon running the program
+
+
+// If/Else/Then Section
