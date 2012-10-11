ccf3c0648b92c2fde81f12e96e432d1d38851629
JessMehaffey <jessmehaffey@fullsail.edu>
2012-10-11 16:36:33 -0300

    Added If Loop

--- Mehaffey_Jessica_Assignment3/Mehaffey_Jessica_Assignment3.js
+++ Mehaffey_Jessica_Assignment3/Mehaffey_Jessica_Assignment3.js
@@ -10,14 +10,32 @@
 
 // Global Variables
 // Used Code-wide
-// Example: var ______ = ""
 
+var NewEmployee = "that new chick"
+var EmployeesScheduled = "6"
+var ScheduleComplete = true
 
 // Output Log
 // This contains the console.log text that will show up upon running the program
 
 
 // If Loop
-
+if (ScheduleComplete === true){
+  if (EmployeesScheduled <= 6){
+		// code that runs if true
+		EmployeesScheduled = EmployeesScheduled + 1;
+		console.log("We have everyone in the schedule, so we can move on to the next task!")
+	} else {
+		// code that runs if false
+		console.log("Better sit down and get that schedule finished!")
+	};
+} else {
+	if (NewEmployee === "that new chick"){
+		console.log("I better double check and make sure " + NewEmployee + " is on this schedule.");
+	} else {
+		NewEmployee = "I really hope " + NewEmployee + " does well. I don\'t want to have to fire her.";
+		console.log(Employee);
+	};
+};
 
 // While Loop
