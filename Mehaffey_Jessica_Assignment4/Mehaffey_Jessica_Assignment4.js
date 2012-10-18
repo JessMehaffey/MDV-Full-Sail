0c5e285a93997fbfd16850c679ee2318187666a8
Jessica Mehaffey <jessmehaffey@fulsail.edu>
2012-10-18 11:49:10 -0400

    Guidelines for Assignment

--- Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
+++ Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
@@ -1,1 +1,23 @@
-alert("JavaScript works!");
+// Jessica Mehaffey
+// SDI 1210
+// October 18, 2012
+// Assignment 4- Function Library
+
+
+// Don't forget semicolons!
+
+// Variables
+
+
+
+// Does string follow pattern (###-###-####) like phone number?
+
+// Does string follow pattern (aaa@bbb.ccc) like an email?
+
+// Is the string a URL? (Start with http: or https:)
+
+// Title-case a string. ((Split into words, then uppercase the first letter of each word)
+
+// Format number to use specific number of decimal places ((like money, 2.1 = 2.10)
+
+// Find the number of hours or days difference between two dates.
