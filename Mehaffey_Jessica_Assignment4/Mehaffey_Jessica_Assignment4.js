ff0972752b0ff2d7cb975e770f30ef147bb1b9ca
Jessica Mehaffey <jessmehaffey@fulsail.edu>
2012-10-18 12:15:20 -0400

    Added "Phone Number Validator".

--- Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
+++ Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
@@ -5,12 +5,33 @@
 
 
 // Don't forget semicolons!
+var phoneNumber = "123-456-7890";
 
-// Variables
+var myLibrary = function(){
+	// Phone Number Validator
+	// Does string follow pattern (###-###-####) like phone number?
+	var checkPhone = function(val){
+	
+		var strPhone = val.length
+		for (var i=0; 1 <=strPhone; i++){
+			if (val.substring(i, i+1) === "-"){
+				return i;
+			}
+		};	
+	
+	};
+
+	return {
+		"checkPhone" : checkPhone,
+		};
+
+};
+
+var newLib = new myLibrary();
+
+console.log("The first dash is in position " + newLib.checkPhone("123-456-7890") + ", and the second dash is in position " + newLib.checkPhone("123-456-7890") + ". Therefore, " + phoneNumber + " is a valid phone number.");
 
 
-
-// Does string follow pattern (###-###-####) like phone number?
 
 // Does string follow pattern (aaa@bbb.ccc) like an email?
 
