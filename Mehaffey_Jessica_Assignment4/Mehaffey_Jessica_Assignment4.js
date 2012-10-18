0d57cac46f6aa2f64aeeba6bc4baf5fadcca05ce
Jessica Mehaffey <jessmehaffey@fulsail.edu>
2012-10-18 12:41:17 -0400

    Added Email Validator

--- Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
+++ Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
@@ -6,15 +6,15 @@
 
 // Don't forget semicolons!
 var phoneNumber = "123-456-7890";
+var eMail = "blahblah@gmail.com";
 
 var myLibrary = function(){
 	// Phone Number Validator
 	// Does string follow pattern (###-###-####) like phone number?
-	var checkPhone = function(val){
-	
-		var strPhone = val.length
+	var checkPhone = function(phone){
+		var strPhone = phone.length
 		for (var i=0; 1 <=strPhone; i++){
-			if (val.substring(i, i+1) === "-"){
+			if (phone.substring(i, i+1) === "-"){
 				return i;
 			}
 		};	
@@ -25,11 +25,27 @@ var myLibrary = function(){
 		"checkPhone" : checkPhone,
 		};
 
+
+	// Email Validator
+	// Does string follow pattern (aaa@bbb.ccc) like email?
+	var checkEmail = function(email){
+		var emailFormat = ([A-Z, a-z, 0-9, _]) + "@" ([A-Z, a-z, 0-9, _]) + "." ([A-Z, a-z]); {2,4};
+		if (emailFormat.test(eMail) === true) {
+			console.log("A valid email address was entered.");
+		} else {
+			console.log("An invalid email address was entered. Please try again.");
+		}
+	};
 };
+
+
+
+
+
 
 var newLib = new myLibrary();
 
-console.log("The first dash is in position " + newLib.checkPhone("123-456-7890") + ", and the second dash is in position " + newLib.checkPhone("123-456-7890") + ". Therefore, " + phoneNumber + " is a valid phone number.");
+console.log("The first dash is in position " + newLib.checkPhone(phoneNumber) + ", and the second dash is in position " + newLib.checkPhone("1230456-7890") + ". Therefore, " + phoneNumber + " is a valid phone number.");
 
 
 
