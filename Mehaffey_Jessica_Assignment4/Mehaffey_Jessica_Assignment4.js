50136ea31d55ca3a3b32cd07b1ee1bbc2d1a6bee
Jessica Mehaffey <jessmehaffey@fullsail.edu>
2012-10-18 13:39:57 -0400

    URL function complete, with minor editing in progress. Working on Title Case Function.

--- Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
+++ Mehaffey_Jessica_Assignment4/Mehaffey_Jessica_Assignment4.js
@@ -7,6 +7,8 @@
 // Don't forget semicolons!
 var phoneNumber = "123-456-7890";
 var eMail = "blahblah@gmail.com";
+var URL = "http://www.blahblah.com";
+var wordsTitleCase = "This has been converted to Title case.";
 
 var myLibrary = function(){
 	// Phone Number Validator
@@ -32,31 +34,48 @@ var myLibrary = function(){
 			console.log("An invalid email address was entered. Please try again.");
 		}
 		
+	
+	// URL Validator
+	// Does string start with "http:" or "https:"?	
+	var checkURL = function(url){
+		if (!(/^http:\/\//.test(url))){
+			console.log("The URL is valid.");
+			}
+		else if (!(/^https:\/\//.test(url))){
+			console.log("The URL is valid.");
+		} else {
+			console.log("The URL is invalid.");
+		};
 		
+	// Title-case a string
+	// Split into words, then uppercase first letter of each word
+	var checkTitleCase = function (titleCase){
+	
+	}
 	};
+	// Decimal Format
+	// Format number to use specific number of decimal places (2.1 to 2.10)
+		
+		
+	// Find Time Difference Between 2 Dates
+	// Find number of hours/days between dates
+		
+};
 	
 	return {
 		"checkPhone" : checkPhone,
-		"checkEmail" : checkEmail
+		"checkEmail" : checkEmail,
+		"checkURL" : checkURL,
+		"checkTitleCase" : checkTitleCase,
 		};
 };
 
 
 
 
-
-
 var newLib = new myLibrary();
 
-console.log("The first dash is in position " + newLib.checkPhone(phoneNumber) + ", and the second dash is in position " + newLib.checkPhone("1230456-7890") + ". Therefore, " + phoneNumber + " is a valid phone number.");
+console.log("The first dash is in position " + newLib.checkPhone(phoneNumber) + ", and the second dash is in position " + newLib.checkPhone("1230456-7890") + ". Therefore, " + phoneNumber + " is a valid phone number.");;
 console.log("The email " + newLib.checkEmail(eMail) + " is valid.");
-
-// Does string follow pattern (aaa@bbb.ccc) like an email?
-
-// Is the string a URL? (Start with http: or https:)
-
-// Title-case a string. ((Split into words, then uppercase the first letter of each word)
-
-// Format number to use specific number of decimal places ((like money, 2.1 = 2.10)
-
-// Find the number of hours or days difference between two dates
+console.log("The URL " + newLib.checkURL(URL) + " is valid.");
+console.log("The string " + newLib.checkTitleCase(titleCase) + " has been converted to Title Case!" );
